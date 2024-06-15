0c62f33ba47f3d3692f6dadc53ec9a06c3cf0e03
Olexandr Shaposhnyk <oshaposhnyk@intelliboard.net>
2024-06-15 23:18:17 +0300

    update migrations

---
+++ schema/000001_init.down.sql
@@ -0,0 +1,11 @@
+-- Deleting table for types
+DROP IF EXISTS type;
+
+-- Deleting table for difficulties
+DROP IF EXISTS difficulty;
+
+-- Deleting table for languages
+DROP IF EXISTS language;
+
+-- Deleting table for library items
+DROP IF EXISTS library_item;
